e9eff50811b78e1fcba81084beb6225eb7e977fa
michleesc <michlee.septian.c@gmail.com>
2022-03-25 17:07:47 +0700

    update users

--- src/validator/users/schema.js
+++ src/validator/users/schema.js
@@ -1,9 +1,9 @@
 const Joi = require('joi');
 
 const UserPayloadSchema = Joi.object({
-    username: Joi.string().required(),
-    password: Joi.number().required(),
-    fullname: Joi.string().required(),
+  username: Joi.string().required(),
+  password: Joi.string().required(),
+  fullname: Joi.string().required(),
 });
 
 module.exports = { UserPayloadSchema };
