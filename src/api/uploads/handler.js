118f7893dafe80afb2a4fa854aa77a9935e39f2e
michleesc <michlee.septian.c@gmail.com>
2022-04-01 21:09:39 +0700

    fix coverUrl

--- src/api/uploads/handler.js
+++ src/api/uploads/handler.js
@@ -16,7 +16,7 @@ class UploadsHandler {
             this._validator.validateImageHeaders(cover.hapi.headers);
 
             const filename = await this._service.writeFile(cover, cover.hapi);
-            const coverUrl = `http://${process.env.HOST}:${process.env.PORT}/albums/${id}/covers/${filename}`;
+            const coverUrl = `http://${process.env.HOST}:${process.env.PORT}/upload/images/${filename}`;
 
             await this._albumsService.addCoverAlbumById(id, coverUrl);
             const response = h.response({
