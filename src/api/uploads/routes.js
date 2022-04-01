8e8c0478adcd1db409ed0c67c4b0cbb16cc67821
michleesc <michlee.septian.c@gmail.com>
2022-04-01 15:34:39 +0700

    local stroge implemented

--- src/api/uploads/routes.js
+++ src/api/uploads/routes.js
@@ -16,7 +16,7 @@ const routes = (handler) => [
     },
     {
         method: 'GET',
-        path: '/upload/{param*}',
+        path: '/albums/{param*}',
         handler: {
           directory: {
             path: path.resolve(__dirname, 'file'),
