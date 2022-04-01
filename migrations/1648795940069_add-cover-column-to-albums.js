041e4d9d9d9f0976db148ce7663003dfd1e24289
michleesc <michlee.septian.c@gmail.com>
2022-04-01 13:54:16 +0700

    add column to albums

---
+++ migrations/1648795940069_add-cover-column-to-albums.js
@@ -0,0 +1,15 @@
+/* eslint-disable camelcase */
+
+exports.shorthands = undefined;
+
+exports.up = (pgm) => {
+    pgm.addColumn('albums', {
+        cover: {
+            type: 'TEXT',
+        },
+    });
+};
+
+exports.down = (pgm) => {
+    pgm.dropColumn('albums', 'cover');
+};
