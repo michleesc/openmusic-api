0b03cea4eb4a3937d2e5c77ccff1f2886428aab9
michleesc <michlee.septian.c@gmail.com>
2022-04-02 09:42:22 +0700

    implemented cache

---
+++ migrations/1648865630318_create-table-user-album-likes.js
@@ -0,0 +1,35 @@
+/* eslint-disable camelcase */
+
+exports.shorthands = undefined;
+
+exports.up = (pgm) => {
+    pgm.createTable('user_album_likes', {
+        id: {
+            type: 'VARCHAR(50)',
+            primaryKey: true,
+        },
+        user_id: {
+            type: 'VARCHAR(50)',
+            notNull: true,
+        },
+        album_id: {
+            type: 'VARCHAR(50)',
+            notNull: true,
+        },
+    });
+    /*
+    Menambahkan constraint UNIQUE, kombinasi dari kolom user_id dan album_id.
+    Guna menghindari duplikasi data antara nilai keduanya.
+    */
+   pgm.addConstraint('user_album_likes', 'unique_user_id_and_album_id', 'UNIQUE(user_id, album_id)');
+   /*
+    Memberikan constraint foreign key pada kolom user_id dan album_id
+    terhadap users.id dan albums.id
+  */
+  pgm.addConstraint('user_album_likes', 'fk_user_album_likes.user_id_users.id', 'FOREIGN KEY(user_id) REFERENCES users(id) ON DELETE CASCADE');
+  pgm.addConstraint('user_album_likes', 'fk_user_album_likes.album_id_albums.id', 'FOREIGN KEY(album_id) REFERENCES albums(id) ON DELETE CASCADE');
+};
+
+exports.down = (pgm) => {
+    pgm.dropTable('user_album_likes');
+};
